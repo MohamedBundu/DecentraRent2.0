fc61813cd328dce8f1d3868c3cb8700dbf6b2fa7
SammyK4488 <skleant1@students.towson.edu>
2025-06-29 17:06:56 -0400

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  VITE_SUPABASE_URL: any
+  VITE_SUPABASE_ANON_KEY: any
+  readonly VITE_APP_TITLE?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+interface ImportMetaEnv {
+  readonly VITE_CONTRACT_ADDRESS: string
+}
